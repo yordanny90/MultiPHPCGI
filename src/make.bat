d63e418bdde9b1721a4c10f0f1688ecd1e6cc86f
ymejias <ymejias@cyberfuel.com>
2026-02-20 15:09:39 -0600

    Mejoras de aplicación
    - Mejora de la aplicación: Agrega icono al MultiPHPCGI.exe
    - Nueva librería openssl

--- src/make.bat
+++ src/make.bat
@@ -1,3 +1,10 @@
 @echo off
-del "%~dp0..\MultiPHPCGI.exe"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /t:winexe "/out:%~dp0..\MultiPHPCGI.exe" "%~dp0TrayIcon.cs"
+setlocal
+set "csc=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
+if not exist "%csc%" (
+    echo Debe instalar "%csc%" para compilar MultiPHPCGI
+    exit /b 1
+)
+set "exe=%~dp0..\MultiPHPCGI.exe"
+del "%exe%"
+"%csc%" /t:winexe "/win32icon:%~dp0..\bin\app\favicon.ico" "/out:%exe%" "%~dp0TrayIcon.cs"
